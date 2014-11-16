3c4036679b0994e1afbc487d370914418e502f69
jcizel <jcizel@gmail.com>
2014-11-16 12:23:11 +0100

    add documentation

--- R/tableProduction.R
+++ R/tableProduction.R
@@ -1,6 +1,21 @@
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Convert a data.table into inner part of latex table (table
+##' environment, column names, rules to be produced manually)
+##' @param dt data.table to be converted into latex table 
+##' @param outfile path of the destination file
+##' @param digits digit formating. Useful only if table consists only of numbers
+##' with the same desired formating. If this is not the case, it is easier to
+##' arrange the formating manually on the original data.table.
+##' @param include.rownames Include rownames? Default is FALSE.
+##' @param include.colnames Include colnames? Default is FALSE
+##' @return Character vector, where each entry corresponds to the row in the
+##' output data table. Main use of this function
+##' is its side product consisting of the 'outfile' latex table.
+##' @author Janko Cizel
 dataTableToInnerLatex <- function(dt,
-                                  file = './Downloads/test.tex',
+                                  outfile = '~/Downloads/test.tex',
                                   digits = 0,
                                   include.rownames = FALSE,
                                   include.colnames = FALSE){
@@ -16,7 +31,7 @@ dataTableToInnerLatex <- function(dt,
     })
     
     LaTeXTableGems:::.innerTEX(out,
-                               file = file)
+                               file = outfile)
 
     return(out)
 }
